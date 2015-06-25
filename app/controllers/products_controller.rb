35fcee9ac3ffe76649d3de61ca61be28889af261
zhangmyihua <michaelzhang2@msn.com>
2015-06-26 06:06:38 +0800

    removed user id from product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,9 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
+    @product.user = current_user
+    @product.save
+    # respond_with(@product)
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -69,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :permalink, :description, :price, :user_id)
+      params.require(:product).permit(:name, :permalink, :description, :price, :file)
     end
 end
