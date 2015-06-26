b2816c595e6a018bc726ab74f129e0c8901e390e
zhangmyihua <michaelzhang2@msn.com>
2015-06-27 02:54:02 +0800

    adding stripe method to create transaction instance using variables stored in the product and the sale

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -12,5 +12,27 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    product = Product.find_by!(permalink: params[:permalink])
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        amount: product.price,
+        currency: 'usd',
+        source: token, 
+        description: params[:stripeEmail]
+        )
+      @sale = product.sales.create!(
+        email: params[:stripeEmail],
+        stripe_id: charge_id
+        )
+       redirect_to pickup_url(guid: @sale.guid) #This line redirects to the url where the user picks up their purchase      
+    
+    rescue: Stripe::CardError => e 
+      # if stripe gets an error, the rescue method stores the error in a variable and redirects to :new route
+      @error = e 
+      render :new
+    end
   end
+
 end
