2846cb87b25133d52f367dcd079305bd74b851c8
zhangmyihua <michaelzhang2@msn.com>
2015-06-27 05:16:27 +0800

    fixing errors

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -28,7 +28,7 @@ class TransactionsController < ApplicationController
         )
        redirect_to pickup_url(guid: @sale.guid) #This line redirects to the url where the user picks up their purchase      
     
-    rescue: Stripe::CardError => e 
+    rescue Stripe::CardError => e 
       # if stripe gets an error, the rescue method stores the error in a variable and redirects to :new route
       @error = e 
       render :new
